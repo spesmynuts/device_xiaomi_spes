5c13c52b266666be9631607d7e3f4b79d99f8325
Jashkirat <jashkiratsingh@gmail.com>
2024-07-25 00:36:39 -0400

    Revert "spes: Sign build with custom keys"
    This reverts commit b4e4fbda38a41445eb5ed815cfe3853f493040b9.

--- device.mk
+++ device.mk
@@ -460,9 +460,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
-# Signed Build
-$(call inherit-product, vendor/extra/product.mk)
-
 # Telephony
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
